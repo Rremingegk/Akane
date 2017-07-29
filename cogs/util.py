6fd5414189cfa31561498d98dbf26ee1cd2d2f1c
StandB <standb@protonmail.com>
2017-07-29 10:04:22 +0200

    about command

--- cogs/util.py
+++ cogs/util.py
@@ -7,11 +7,17 @@ class Util:
 	def __init__(self, bot):
 		self.bot = bot
 
+	@commands.command()
+	async def about(self):
+		await self.bot.say("This bot has been made by StandB out of boredom. You can check out the source on github https://github.com/StandB/Akane")
+
 	@commands.command(pass_context=True)
 	async def choose(self, ctx):
 		options = ctx.message.content.split(" ")[1:]
 		decision = random.choice(options)
 		await self.bot.say(f"Decision: {decision}")
+
+
 
 def setup(bot):
 	bot.add_cog(Util(bot))
