b39a85fabf8154c1fcbbfcbc600b9b8bbc6ff704
StandB <standb@protonmail.com>
2017-08-10 18:34:05 +0200

    added ugly logging

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, logging
 
 import discord
 from discord.ext import commands
@@ -10,6 +10,19 @@ command_prefix = '~'
 description = 'My own weeb bot'
 cogs = ['cogs.osu', 'cogs.mal', 'cogs.reddit', 'cogs.radio', 'cogs.util', 'cogs.kitsu', 'cogs.akane']
 
+#logging.basicConfig(level=logging.INFO, filename='discord.log')
+
+disc_logger = logging.getLogger('discord')
+disc_logger.setLevel(logging.DEBUG)
+disc_handler = logging.FileHandler('discord.log', 'w', 'utf-8')
+#disc_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+disc_logger.addHandler(disc_handler)
+
+logger = logging.getLogger('commands')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('errors.log', 'w', 'utf-8')
+logger.addHandler(handler)		
+
 class Akane(Bot):
 
 	def __init__(self, command_prefix, **options):
@@ -18,6 +31,12 @@ class Akane(Bot):
 	@staticmethod
 	async def on_ready():
 		print("Online!")
+
+	async def on_command_error(self, exception, ctx):
+		if isinstance(exception, commands.errors.CommandNotFound):
+			logger = logging.getLogger('commands')
+			logger.debug(f'{ctx.message.author} - {ctx.message.server.name} - {ctx.message.content} - {exception}')
+			
 
 
 if __name__ == '__main__':
